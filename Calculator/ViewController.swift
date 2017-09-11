eee2a9c66230375ac67a5b95806effc26ad0881c
pihanhsu <pihanhsu@gmail.com>
2017-09-11 18:58:01 +0800

    add press number function with all conditions

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,26 +10,83 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    // Labels Outlet
+    // MARK: Labels Outlet
     @IBOutlet weak var chineseNumberLabel: UILabel!
     @IBOutlet weak var displayLabel: UILabel!
     @IBOutlet weak var pendingOperationLabel: UILabel!
     
+    //MARKS: local variables
+    private var displayString: String {
+        get{
+            return displayLabel.text!
+        }
+        set {
+            displayLabel.text = newValue
+        }
+    }
+    
+    private var displayChars: Array<Character> {
+        get {
+            return Array(displayString)
+        }
+        set {
+            displayString = String(newValue)
+            
+        }
+    }
+    
+    private var hasDot = false
+    var displayValue: Double{
+        get {
+            return Double(displayString)!
+        }
+        set {
+            displayString = String(newValue)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-    @IBAction func numberPressed(_ sender: UIButton) {
-    }
     
+    // MARK: IBActions
+    @IBAction func numberPressed(_ sender: UIButton) {
+        
+        
+        if sender.currentTitle == "." {
+            if hasDot {
+                return
+            }else{
+                hasDot = true
+            }
+        }
+        
+        if (displayChars[0] == "0" && displayChars.count == 1){
+            if(sender.currentTitle == "."){
+                displayString += sender.currentTitle!
+                hasDot = true
+            }else{
+                displayString = sender.currentTitle!
+            }
+        }else{
+            displayString += sender.currentTitle!
+        }
+    }
     
     @IBAction func performOperation(_ sender: UIButton) {
     }
     
     @IBAction func backspacePressed(_ sender: Any) {
+        if displayChars.count == 1 {
+            displayChars[0] = "0"
+        }else{
+            displayChars.removeLast()
+        }
     }
     
     @IBAction func resetCalculator(_ sender: UIButton) {
+        displayString = "0"
+        hasDot = false
     }
     
 
